c2bd8f5210fc6ec9b0e38f14331c1b7c9b6ad8d2
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-03-26 13:23:37 +0000

    add mrshmallow schema and sqlite3 for dev

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,9 @@ from distutils.debug import DEBUG
 
 class APIProdConfig:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
 
 class APITestConfig:
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
