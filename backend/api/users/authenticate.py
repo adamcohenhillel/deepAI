bcce4c6371f62f8129fcaaad6b7b9389324f457e
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-06-26 18:25:08 +0100

    generate jwt works

--- backend/api/users/authenticate.py
+++ backend/api/users/authenticate.py
@@ -2,22 +2,25 @@
 """
 from sanic import Request
 from sanic_jwt.exceptions import AuthenticationFailed
-from users.models import User
+from api.users.models import User
+from sqlalchemy.future import select
 
 
 async def authenticate(request: Request):
-    """
+    """Authentricate user based on username and password
     """
     username = request.json.get('username', None)
     password = request.json.get('password', None)
 
-    if not username or not password:
+    if not (username or password):
         raise AuthenticationFailed('Missing username or password')
 
     async with request.ctx.session.begin():
-        user = await User.get(username=username)
+        query = select(User).where(User.username == username)
+        result = await request.ctx.session.execute(query)
+        user: User = result.scalars().first()
         
     if user is None or password != user.password:
         raise AuthenticationFailed("Username or password are incorrect")
     else:
-        return dict(user)
+        return {'user_id': user.id, 'username': user.username}
