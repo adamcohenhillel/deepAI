7ca7ee8f8d01000615877fbf6c45aa5bda92d36d
Adam Cohen Hillel <adamcohenhillel@gmail.com>
2022-07-09 12:17:05 +0100

    try one test

--- backend/tests/unit/test_api/test_deeprequest.py
+++ backend/tests/unit/test_api/test_deeprequest.py
@@ -3,12 +3,12 @@
 from tests.fixtures import generic_test_setup, GenericTestSetup
     
 
-async def test_create_new_deep_request(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, access_token = generic_test_setup
-    _, response = await api_app.asgi_client.post(
-        '/v1/deeprequest',
-        headers={'Authorization': f'Bearer {access_token}'},
-        json={'deep_request': 'This is something deep'}
-    )
-    assert response.status == 201
-    assert isinstance(response.json['node'], int)
+# async def test_create_new_deep_request(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, access_token = generic_test_setup
+#     _, response = await api_app.asgi_client.post(
+#         '/v1/deeprequest',
+#         headers={'Authorization': f'Bearer {access_token}'},
+#         json={'deep_request': 'This is something deep'}
+#     )
+#     assert response.status == 201
+#     assert isinstance(response.json['node'], int)
--- backend/tests/unit/test_api/test_users.py
+++ backend/tests/unit/test_api/test_users.py
@@ -13,58 +13,58 @@ async def test_create_new_user_success(generic_test_setup: GenericTestSetup) -> 
     assert response.json == {'message': 'Created'}
 
 
-async def test_create_new_user_existing_username(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post(
-        '/v1/users',
-        json={'password': 'Aa12345678@', 'username': 'test_user'}
-    )
-    assert response.status == 400
+# async def test_create_new_user_existing_username(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post(
+#         '/v1/users',
+#         json={'password': 'Aa12345678@', 'username': 'test_user'}
+#     )
+#     assert response.status == 400
 
 
-async def test_create_new_user_week_password(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post(
-        '/v1/users',
-        json={'password': 'week', 'username': 'banana'}
-    )
-    assert response.status == 400
+# async def test_create_new_user_week_password(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post(
+#         '/v1/users',
+#         json={'password': 'week', 'username': 'banana'}
+#     )
+#     assert response.status == 400
 
 
-async def test_create_new_user_bad_schema(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post(
-        '/v1/users',
-        json={'password': 'week', 'woops': 'banana'}
-    )
-    assert response.status == 400
+# async def test_create_new_user_bad_schema(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post(
+#         '/v1/users',
+#         json={'password': 'week', 'woops': 'banana'}
+#     )
+#     assert response.status == 400
 
 
-async def test_authentication_success(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post('/v1/auth', json={'password': 'Aa12345678!', 'username': 'test_user'})
-    assert 'access_token' in response.json
+# async def test_authentication_success(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post('/v1/auth', json={'password': 'Aa12345678!', 'username': 'test_user'})
+#     assert 'access_token' in response.json
 
 
-async def test_authentication_bad_username(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post('/v1/auth', json={'password': '12345678', 'username': 'notexists'})
-    assert response.status == 401
+# async def test_authentication_bad_username(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post('/v1/auth', json={'password': '12345678', 'username': 'notexists'})
+#     assert response.status == 401
 
 
-async def test_authentication_bad_password(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post('/v1/auth', json={'password': 'badpass', 'username': 'test_user'})
-    assert response.status == 401
+# async def test_authentication_bad_password(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post('/v1/auth', json={'password': 'badpass', 'username': 'test_user'})
+#     assert response.status == 401
 
 
-async def test_authentication_bad_body_schema(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post('/v1/auth', json={'notpassword': '12345678', 'username': 'test_user'})
-    assert response.status == 401
+# async def test_authentication_bad_body_schema(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post('/v1/auth', json={'notpassword': '12345678', 'username': 'test_user'})
+#     assert response.status == 401
 
 
-async def test_authentication_no_body_schema(generic_test_setup: GenericTestSetup) -> None:
-    api_app, _, _ = generic_test_setup
-    _, response = await api_app.asgi_client.post('/v1/auth')
-    assert response.status == 400
+# async def test_authentication_no_body_schema(generic_test_setup: GenericTestSetup) -> None:
+#     api_app, _, _ = generic_test_setup
+#     _, response = await api_app.asgi_client.post('/v1/auth')
+#     assert response.status == 400
